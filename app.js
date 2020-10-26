8a859f699f01bdd47b403c1c7ce89e5e992ffda2
chavangorakh1999 <chavangorakh19999@ggmail.com>
2020-10-26 13:07:10 +0530

    styling and creating pages passing items to list

--- app.js
+++ app.js
@@ -1,23 +1,58 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { urlencoded } = require("body-parser");
 
 const app = express();
 
+app.use(express.static("public"));
+let items=["Buy Food"];
+let workItems=[];
+
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", (req, res) => {
-  var today = new Date();
-  var presentDay = today.getDay();
-  var day = "";
-  var week=["sunday","monday","tuesday","wednesday","thursday","friday","saturday"];
-    day = week[presentDay];
+  let today = new Date();
+  
+  let options ={
+    weekday:"long",
+    day:"numeric",
+    month:"long",
+  };
+  let day = today.toLocaleDateString("en-US",options);
   res.render("list", {
-    kindOfDay: day,
+    listTitle: day,
+    newListItems:items
   });
 
 });
 
-var port = process.env.PORT || 3000;
+app.post("/",(req,res) => {
+
+  let item=req.body.listItem;
+  if(req.body.list === "Work")
+  {
+    workItems.push(item);
+    res.redirect("/work");
+  }else{
+    items.push(item);
+    res.redirect("/");
+  }
+  
+});
+
+app.get("/work",(req,res) => {
+  res.render("list", {
+    listTitle:"Work List",
+    newListItems:workItems
+  });
+});
+
+app.get("/about",(req,res) => {
+  res.render("about");
+});
+
+let port = process.env.PORT || 3000;
 app.listen(port, (req, res) => {
   console.log("Server is running on port " + port);
 });
