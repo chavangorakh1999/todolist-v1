f8612fef9bd91e57f0a443cf1881255d028df219
chavangorakh1999 <chavangorakh19999@ggmail.com>
2020-10-26 22:17:48 +0530

    exporting and including modules

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const { urlencoded } = require("body-parser");
+const date = require (__dirname+"/date.js");
+
 
 const app = express();
 
@@ -12,18 +13,12 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  let today = new Date();
-
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-  };
-  let day = today.toLocaleDateString("en-US", options);
+  let day = date.getDate();
   res.render("list", {
     listTitle: day,
     newListItems: items,
   });
+  
 });
 
 app.post("/", (req, res) => {
