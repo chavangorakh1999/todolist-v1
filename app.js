8c94e7539241b602888a9f15a650442990dffdbe
chavangorakh1999 <chavangorakh19999@ggmail.com>
2020-10-30 15:24:20 +0530

    connected to mongodb database

--- app.js
+++ app.js
@@ -1,42 +1,112 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require (__dirname+"/date.js");
 
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/todolistDB", {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
 
 const app = express();
 
 app.use(express.static("public"));
-let items = ["Buy Food"];
-let workItems = [];
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const itemSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Enter event"],
+  },
+});
+
+const workItem = mongoose.model("workItem", itemSchema);
+const Item = mongoose.model("Item", itemSchema);
+
+const item1 = new Item({
+  name: "Welcome to list",
+});
+
+const item2 = new Item({
+  name: "Press + icon to add to Item",
+});
+const item3 = new Item({
+  name: "Press - icon to delete to Item",
+});
+
+const defaultItems = [item1, item2, item3];
+
 app.get("/", (req, res) => {
-  let day = date.getDate();
-  res.render("list", {
-    listTitle: day,
-    newListItems: items,
+  Item.find({}, (err, foundItems) => {
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems, (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("items inserted sucessfully");
+        }
+      });
+      res.redirect("/");
+    } else {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("list", {
+          listTitle: "Today",
+          newListItems: foundItems,
+        });
+      }
+    }
   });
-  
 });
 
 app.post("/", (req, res) => {
   let item = req.body.listItem;
   if (req.body.list === "Work") {
-    workItems.push(item);
+    workItem.create({name:item},(err)=>{
+      if(err){
+        console.log(err);
+      }else{
+        console.log("work item added sucessfully");
+      }
+    });
     res.redirect("/work");
   } else {
-    items.push(item);
+    Item.create({ name: item }, (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("item updated sucessfully");
+      }
+    });
     res.redirect("/");
   }
 });
 
 app.get("/work", (req, res) => {
-  res.render("list", {
-    listTitle: "Work List",
-    newListItems: workItems,
+  workItem.find((err, foundworkItems) => {
+    if (foundworkItems.length === 0) {
+      workItem.insertMany(defaultItems, (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Inserted documnent sucessfully");
+        }
+      });
+    } else {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("list", {
+          listTitle: "Work",
+          newListItems: foundworkItems,
+        });
+      }
+    }
   });
+  
 });
 
 app.get("/about", (req, res) => {
